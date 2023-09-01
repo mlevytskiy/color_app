94202acbc356a42d82a8c0222af1c547fa8d776e
Maksym <m.levytskiy@gmail.com>
2023-09-01 22:01:20 +0300

    fix warning

--- lib/util/color_brightness_detector_extension.dart
+++ lib/util/color_brightness_detector_extension.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 ///extension helps to detect if current color bright or not
-extension ColorBritnessDetectorExtension on Color {
+extension ColorBrightnessDetectorExtension on Color {
   ///just for determinate if it is color closer for dark color or for light.
   ///If it is exactly 0.5 - it is mean that color brightness is in the middle.
   static const double middleBrightness = 0.5;
